4d1fb517a3fd87d2d1cbdbef2c88192ad23ed1b5
IreneQuero <queroirene@gmail.com>
2022-11-20 00:43:23 +0000

    erandom card generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,53 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let nombresIconos = ["pica", "diamante", "corazones", "trebol"];
+  let iconos = {
+    pica: "♠",
+    diamante: "♦",
+    corazones: "♥",
+    trebol: "♣"
+  };
+  const numerosRandom = arr => {
+    let numero = Math.round(Math.random() * (arr.length - 1));
+    return arr[numero];
+  };
+
+  let tipo = numerosRandom(nombresIconos);
+  let carta = numerosRandom(numeros);
+
+  let iconoTop = document.querySelector(".icono-superior");
+  iconoTop.innerHTML = iconos[tipo];
+  iconoTop.classList.add("tipo");
+
+  let numerosOtro = document.querySelector(".numero");
+  numerosOtro.innerHTML = carta;
+  numerosOtro.classList.add("carta");
+
+  let iconoBot = document.querySelector(".icono-inferior");
+  iconoBot.innerHTML = iconos[tipo];
+  iconoBot.classList.add("tipo");
+
+  if (nombresIconos === "diamante" || nombresIconos === "corazones") {
+    iconoTop.style.color = "red";
+    numerosOtro.style.color = "red";
+    iconoBot.style.color = "red";
+  }
 };
